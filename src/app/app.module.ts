8f26599aad32365f960a947346ae1d3388519076
Larry Caichihua Vergara <lcaichihua@gmail.com>
2019-04-15 01:31:37 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { MatModule } from './mat/mat.module';
+import { ListCustomersComponent } from './components/list-customers/list-customers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListCustomersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
+    BrowserAnimationsModule,
+    MatModule
   ],
   providers: [],
   bootstrap: [AppComponent]
